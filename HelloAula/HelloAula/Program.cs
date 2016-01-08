71d0820dfb9007b084aa35ab73ce9365feea12d3
Ivan Yordanov <iv.yordanov@mail.bg>
2016-01-08 15:00:53 +0200

    New corection

---
+++ HelloAula/HelloAula/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloAula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello Aula!");
+			Console.ReadKey ();
+
+
+		}
+	}
+}
