4d3dc0b6d17036bf68df0ba374078c189a8aaeac
Ivan Yordanov <iv.yordanov@mail.bg>
2016-01-09 09:23:31 +0200

    Модул 01 Видео 02-Променливи от тип int

--- HelloAula/HelloAula/Program.cs
+++ HelloAula/HelloAula/Program.cs
@@ -8,11 +8,18 @@ namespace HelloAula
 		{
 			
 			// Дефиниране на променливи
-
+			int a =0,b=0;
+			int sum = 0;
 			// Събиране на числа
-
+			a=5;
+			b = 10;
+			sum = a + b;
 			// Печатане в конзолата
-
+			Console.Write(a);
+			Console.Write ("+");
+			Console.Write (b);
+			Console.Write ("=");
+			Console.WriteLine (sum);
 
 		}
 	}
