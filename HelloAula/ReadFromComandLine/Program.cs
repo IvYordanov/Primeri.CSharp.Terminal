67966b0ad82d766916383da5cb92fb7ed7f6263f
Ivan Yordanov <iv.yordanov@mail.bg>
2016-01-10 20:37:21 +0200

    Модул 02,Видео 06- Работа със стрингове

--- HelloAula/ReadFromComandLine/Program.cs
+++ HelloAula/ReadFromComandLine/Program.cs
@@ -7,7 +7,7 @@ namespace ReadFromComandLine
 		public static void Main (string[] args)
 		{
 			//Дефиниране на параметри
-			int a =5,b=0;
+			int a = 5, b = 0;
 
 			//Въвеждане на фходни параметри
 			Console.WriteLine ("Моля въведете а:  ");
@@ -16,7 +16,20 @@ namespace ReadFromComandLine
 
 			//Печатане на резултат
 
-			Console.WriteLine("Резултата  (a+b) е:  "+(a+b).ToString()+"\n\n\n\n");
+			Console.WriteLine ("Резултата  (a+b) е:  " + (a + b).ToString () + "\n\n\n\n");
+
+			string test = "1";
+			test += ",2";
+			test += ",100";
+			test += ",4";
+			test += ",5";
+			Console.WriteLine ("Събиране  с +=  :  " + test + "\n");
+			// Заместване на символите , ;
+			test = test.Replace (",", ";");
+			Console.WriteLine ("Работа с Replace: "+test+"\n");
+			Console.WriteLine ("Третия знак е: " + test.Split(';')[2]);
+
+
 		}
 	}
 }
