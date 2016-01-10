2c17ecb52b45164f6040d24c69baf74955290b7f
Ivan Yordanov <iv.yordanov@mail.bg>
2016-01-10 18:35:17 +0200

    Модул 02 , Видео 05 - Четене от ред

---
+++ HelloAula/ReadFromComandLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ReadFromComandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на параметри
+			int a =5,b=0;
+
+			//Въвеждане на фходни параметри
+			Console.WriteLine ("Моля въведете а:  ");
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+
+			//Печатане на резултат
+
+			Console.WriteLine("Резултата  (a+b) е:  "+(a+b).ToString()+"\n\n\n\n");
+		}
+	}
+}
---
+++ modul02/modul02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace modul02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("     Hello CSharp! \n\n");
+            int a = 456;
+            int b = 6;
+            decimal g = 0.65558m;
+            var sum = a + b / g;
+            Console.WriteLine(sum);
+            string name =  "hello my programe" ;
+            Console.WriteLine(name);
+        }
+    }
+}
