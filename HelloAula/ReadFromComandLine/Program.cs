e07e8f0e1d508ae0b0c1df8e95158ac038a391d7
Ivan Yordanov <iv.yordanov@mail.bg>
2016-01-10 22:50:34 +0200

    Модул 02 ,Видео 07 -Работа с масиви

--- HelloAula/ReadFromComandLine/Program.cs
+++ HelloAula/ReadFromComandLine/Program.cs
@@ -27,7 +27,40 @@ namespace ReadFromComandLine
 			// Заместване на символите , ;
 			test = test.Replace (",", ";");
 			Console.WriteLine ("Работа с Replace: "+test+"\n");
-			Console.WriteLine ("Третия знак е: " + test.Split(';')[2]);
+			Console.WriteLine ("Третия знак е: " + test.Split(';')[2]+"\n\n");
+
+
+			//Задача №1
+			/*Декларирайте две променливи от тип string и им присвоете стойности "Hello" и "World". 
+			 * Декларирайте променлива от тип object и й присвоете стойността на конкатенацията на двете променливи от тип string
+			 * (не изпускайте интервала по средата).
+			 * Декларирайте трета променлива от тип string
+			 * и я инициализирайте със стойността на променливата от тип object. ( Hint: Трябва да използвате type casting.)
+           */
+			string first = "Hello",second="World!";
+			object all = first +" "+ second;
+			//string third = all;
+			Console.WriteLine (all+"\n\n");
+			//Задачата не е решена СЛЕДВА ПРОДЪЛЖЕНИЕ!!!!!
+
+			//Задача№2
+			/*Да се въведат от командния ред 1 число или цифра и 1 дума/стринг.
+			 * След това да изведе дължината на получения стринг при конкатенация,
+			 * тоест броя на символите в новополучения стринг при конкатенация.
+			 * (Hint: Трябва да преобразувате int към String и след това да конкатениратe.)
+			 */
+			Console.Write (" Enter number: ");
+			int number = Convert.ToInt32 (Console.ReadLine ());
+			Console.Write (" Enter word: ");
+			string word = Console.ReadLine ();
+			string conc = Convert.ToString (number);
+			string arr = number + word; 
+			Console.WriteLine (arr);
+			Console.WriteLine (" Брой символи:  "+arr.Length+"\n\n");
+			//Задачата е решена
+
+
+
 
 
 		}
